8c6cea180ea5b19bd3b17af839c747afc8c9e8f1
leSamo <soleksak@redhat.com>
2022-03-08 19:24:27 +0100

    Add dummy CVE list page

---
+++ src/Components/SmartComponents/CveListPage.js
@@ -0,0 +1,67 @@
+import React, { Fragment } from 'react';
+import { Alert, AlertVariant, Popover } from '@patternfly/react-core';
+import {
+  OutlinedQuestionCircleIcon,
+  ExternalLinkAltIcon,
+} from '@patternfly/react-icons';
+import { Main } from '@redhat-cloud-services/frontend-components/Main';
+import {
+  PageHeader,
+  PageHeaderTitle,
+} from '@redhat-cloud-services/frontend-components/PageHeader';
+import CveTable from './CveTable';
+
+const CveListPage = () => {
+  // TODO: Add correct link
+  const PRODUCT_DOC = 'https://access.redhat.com/';
+
+  const HeaderTitle = (
+    <Popover
+      enableFlip
+      position="right"
+      bodyContent={
+        <Fragment>
+          The Vulnerability service identifies CVEs with errata that may affect
+          your Insights-connected OpenShift clusters.
+          <br />
+          <br />
+          Vulnerability information applies for OCP4.8+ version only.
+        </Fragment>
+      }
+      footerContent={
+        <a href={PRODUCT_DOC} target="__blank" rel="noopener noreferrer">
+          Learn more <ExternalLinkAltIcon />
+        </a>
+      }
+    >
+      <span>
+        CVEs
+        <OutlinedQuestionCircleIcon
+          color="var(--pf-global--secondary-color--100)"
+          className="pf-u-ml-sm pointer cves-header-questionmark"
+          style={{ verticalAlign: '0' }}
+        />
+      </span>
+    </Popover>
+  );
+
+  // TODO: The alert should be dismissable (actionClose prop)
+  return (
+    <Fragment>
+      <PageHeader>
+        <PageHeaderTitle title={HeaderTitle} />
+        <Alert
+          variant={AlertVariant.info}
+          isInline
+          className="pf-u-mt-md"
+          title="Vulnerability information applies to OCP4.8+ version only"
+        />
+      </PageHeader>
+      <Main>
+        <CveTable />
+      </Main>
+    </Fragment>
+  );
+};
+
+export default CveListPage;
---
+++ src/Components/SmartComponents/CveTable.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import SkeletonTable from '@redhat-cloud-services/frontend-components/SkeletonTable/SkeletonTable';
+
+const CveTable = () => {
+  return <SkeletonTable colSize={6} rowSize={20} />;
+};
+
+export default CveTable;
