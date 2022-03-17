bbffd75b79d2717e7655eeaac9769e648eddc383
Samuel Olekšák <soleksak@redhat.com>
2022-03-17 04:20:56 -0400

    Add breadcrumbs (#3)

--- src/Components/SmartComponents/ClusterDetailPage.js
+++ src/Components/SmartComponents/ClusterDetailPage.js
@@ -6,11 +6,22 @@ import {
 } from '@redhat-cloud-services/frontend-components/PageHeader';
 import CveTable from './CveTable';
 import PropTypes from 'prop-types';
+import { Breadcrumb, BreadcrumbItem } from '@patternfly/react-core';
+import { Link } from 'react-router-dom';
 
 const ClusterDetailPage = ({ match }) => {
   return (
     <Fragment>
       <PageHeader>
+        <Breadcrumb>
+          <BreadcrumbItem>
+            <Link to="/">Vulnerability</Link>
+          </BreadcrumbItem>
+          <BreadcrumbItem>
+            <Link to="/clusters">Clusters</Link>
+          </BreadcrumbItem>
+          <BreadcrumbItem>{match.params.clusterId}</BreadcrumbItem>
+        </Breadcrumb>
         <PageHeaderTitle title={match.params.clusterId} />
       </PageHeader>
       <Main>
--- src/Components/SmartComponents/CveDetailPage.js
+++ src/Components/SmartComponents/CveDetailPage.js
@@ -6,11 +6,22 @@ import {
 } from '@redhat-cloud-services/frontend-components/PageHeader';
 import ClusterTable from './CveTable';
 import PropTypes from 'prop-types';
+import { Breadcrumb, BreadcrumbItem } from '@patternfly/react-core';
+import { Link } from 'react-router-dom';
 
 const CveDetailPage = ({ match }) => {
   return (
     <Fragment>
       <PageHeader>
+        <Breadcrumb>
+          <BreadcrumbItem>
+            <Link to="/">Vulnerability</Link>
+          </BreadcrumbItem>
+          <BreadcrumbItem>
+            <Link to="/cves">CVEs</Link>
+          </BreadcrumbItem>
+          <BreadcrumbItem>{match.params.cveId}</BreadcrumbItem>
+        </Breadcrumb>
         <PageHeaderTitle title={match.params.cveId} />
       </PageHeader>
       <Main>
