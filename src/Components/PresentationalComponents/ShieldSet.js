07ea4aa122393298baee15ac167c561185b4bf20
Samuel Olekšák <soleksak@redhat.com>
2022-03-17 04:55:17 -0400

    Add Shield set component (#4)

---
+++ src/Components/PresentationalComponents/ShieldSet.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { SEVERITY_OPTIONS } from '../../constants';
+import { SecurityIcon } from '@patternfly/react-icons';
+import { Link } from 'react-router-dom';
+
+// TODO: Setup link to navigate to Cluster detail page with severity filter applied
+const ShieldSet = ({ count }) => {
+  const DISABLED_COLOR = 'var(--pf-global--disabled-color--200)';
+
+  return (
+    <div className="shield-set">
+      {Object.entries(SEVERITY_OPTIONS).map(
+        ([severityValue, severityProps]) =>
+          severityProps.isInShieldSet &&
+          (count[severityValue] === 0 ? (
+            <Link key={severityValue} className="disabled-shield">
+              <SecurityIcon style={{ color: DISABLED_COLOR }} />
+              <span>0</span>
+            </Link>
+          ) : (
+            <Link key={severityValue}>
+              <SecurityIcon style={{ color: severityProps.iconColor }} />
+              <span>{count[severityValue]}</span>
+            </Link>
+          ))
+      )}
+    </div>
+  );
+};
+
+ShieldSet.propTypes = {
+  count: PropTypes.shape({
+    critical: PropTypes.number,
+    important: PropTypes.number,
+    moderate: PropTypes.number,
+    low: PropTypes.number,
+  }).isRequired,
+};
+
+export default ShieldSet;
