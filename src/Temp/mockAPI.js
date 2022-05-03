b8daa3274b1981e3aba47b9a7395d3ed1ab4533e
leSamo <soleksak@redhat.com>
2022-05-03 21:04:12 +0200

    Add mock API endpoints

--- src/Temp/mockAPI.js
+++ src/Temp/mockAPI.js
@@ -2,8 +2,33 @@
 // Delete this file along with mockEndpoints/ folder
 // API endpoints have been implemented properly.
 
-import CVES from './mockEndpoints/cves.json';
+import cves from './mockEndpoints/cves.json';
+import cveDetails from './mockEndpoints/cveDetails.json';
+import clusters from './mockEndpoints/clusters.json';
+import clusterDetails from './mockEndpoints/clusterDetails.json';
+import exposedClusters from './mockEndpoints/exposedClusters.json';
+import clusterCves from './mockEndpoints/clusterCves.json';
 
 export const fetchCves = () => {
-  return CVES;
+  return cves;
+};
+
+export const fetchCveDetails = () => {
+  return cveDetails;
+};
+
+export const fetchClusters = () => {
+  return clusters;
+};
+
+export const fetchClusterDetails = () => {
+  return clusterDetails;
+};
+
+export const fetchExposedClusters = () => {
+  return exposedClusters;
+};
+
+export const fetchClusterCves = () => {
+  return clusterCves;
 };
