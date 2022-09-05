15a4e9b8965882c2a4a961de84e45f06dbe82dd6
Alexandr Čelakovský <acelakov@redhat.com>
2022-09-05 13:46:24 +0200

    Upgrade cypress to version 10

---
+++ cypress.config.js
@@ -0,0 +1,31 @@
+const { defineConfig } = require('cypress');
+const webpackConfig = require('./config/cypress.webpack.config.js');
+const codeCoverageTask = require('@cypress/code-coverage/task');
+const {
+  initPlugin,
+} = require('@frsource/cypress-plugin-visual-regression-diff/plugins');
+
+module.exports = defineConfig({
+  env: {
+    pluginVisualRegressionUpdateImages: true,
+    pluginVisualRegressionDiffConfig: {
+      threshold: 0.01,
+    },
+  },
+  viewportWidth: 1000,
+  viewportHeight: 660,
+  video: false,
+  component: {
+    devServer: {
+      framework: 'react',
+      bundler: 'webpack',
+      webpackConfig,
+    },
+    specPattern: 'src/**/*.cy.{js,ts,jsx,tsx}',
+    setupNodeEvents(on, config) {
+      codeCoverageTask(on, config);
+      initPlugin(on, config);
+      return config;
+    },
+  },
+});
