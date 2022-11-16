395648b5302ceaaa4821320c8f236742cd8ad9a9
Aleksandr Voznesenskii <62722417+Fewwy@users.noreply.github.com>
2022-11-16 14:32:17 +0100

    feat(codecov):fixing the travis command for codecoverage (#36)
    * feat(codecov):fixing the travis command for codecoverage
    
    * feat(codecov): adding codecoverage and github actions for cypress
    
    * feat(codecov): updating the codeql to the second version
    
    * feat(codecov):improved yaml files as was advised

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.js', '!src/**/stories/*'],
-  coverageDirectory: './coverage/',
+  collectCoverageFrom: ['src/**/*.{js,jsx}', '!**/node_modules/**'],
+  coverageDirectory: './coverage',
   moduleNameMapper: {
     '\\.(css|scss)$': 'identity-obj-proxy',
   },
