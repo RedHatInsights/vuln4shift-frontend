52855494a78e4d9b100ec55be4a24c272cce6c69
leSamo <soleksak@redhat.com>
2024-04-02 17:47:25 +0200

    Separate element for Tabs to avoid code duplication

--- src/Components/SmartComponents/ClusterDetail/ClusterDetailPage.js
+++ src/Components/SmartComponents/ClusterDetail/ClusterDetailPage.js
@@ -1,20 +1,17 @@
 import React, { useEffect } from 'react';
 import propTypes from 'prop-types';
 import { Main } from '@redhat-cloud-services/frontend-components/Main';
-import { Tab, TabTitleText, Tabs } from '@patternfly/react-core';
 import ClusterCveTable from './ClusterCveTable';
 import ClusterDetailPageHeader from './ClusterDetailPageHeader';
 import { useSelector } from 'react-redux';
 import ErrorHandler from '../../PresentationalComponents/ErrorHandler';
 import useChrome from '@redhat-cloud-services/frontend-components/useChrome';
 import ClusterImagesTable from './ClusterImagesTable';
-import { Link, useLocation } from 'react-router-dom';
-import { urlChangeTab } from '../../../Helpers/miscHelper';
 import { CLUSTER_DETAIL_TABS } from '../../../Helpers/constants';
+import TableTabs from '../../PresentationalComponents/TableTabs';
 
 const ClusterDetailPage = ({ activeTab }) => {
   const chrome = useChrome();
-  const location = useLocation();
 
   const { error, cluster } = useSelector(
     ({ ClusterDetailStore }) => ClusterDetailStore
@@ -31,44 +28,28 @@ const ClusterDetailPage = ({ activeTab }) => {
     <ErrorHandler error={error}>
       <ClusterDetailPageHeader />
       <Main>
-        {activeTab == CLUSTER_DETAIL_TABS.cves ? (
-          <Tabs className="pf-m-light pf-v5-c-table" activeKey={0}>
-            <Tab eventKey={0} title={<TabTitleText>CVEs</TabTitleText>}>
-              <ClusterCveTable />
-            </Tab>
-            <Link
-              style={{ textDecoration: 'none', color: 'black' }}
-              to={urlChangeTab(location.pathname, CLUSTER_DETAIL_TABS.images)}
-            >
-              <Tab
-                eventKey={1}
-                title={<TabTitleText>Exposed images</TabTitleText>}
-              />
-            </Link>
-          </Tabs>
-        ) : (
-          <Tabs className="pf-m-light pf-v5-c-table" activeKey={1}>
-            <Link
-              style={{ textDecoration: 'none', color: 'black' }}
-              to={urlChangeTab(location.pathname, CLUSTER_DETAIL_TABS.cves)}
-            >
-              <Tab eventKey={0} title={<TabTitleText>CVEs</TabTitleText>} />
-            </Link>
-            <Tab
-              eventKey={1}
-              title={<TabTitleText>Exposed images</TabTitleText>}
-            >
-              <ClusterImagesTable />
-            </Tab>
-          </Tabs>
-        )}
+        <TableTabs
+          activeTab={activeTab}
+          tabs={[
+            {
+              title: 'CVEs',
+              path: CLUSTER_DETAIL_TABS.cves,
+              Component: ClusterCveTable,
+            },
+            {
+              title: 'Exposed images',
+              path: CLUSTER_DETAIL_TABS.images,
+              Component: ClusterImagesTable,
+            },
+          ]}
+        />
       </Main>
     </ErrorHandler>
   );
 };
 
 ClusterDetailPage.propTypes = {
-  activeTab: propTypes.oneOf(Object.values(CLUSTER_DETAIL_TABS)),
+  activeTab: propTypes.oneOf(Object.values(CLUSTER_DETAIL_TABS)).isRequired,
 };
 
 export default ClusterDetailPage;
