d2266ba60bbf02ff80a0754be6460e411b062f7b
leSamo <soleksak@redhat.com>
2024-04-18 01:14:35 +0200

    Update CVE detail page to support TableTabs

--- src/Components/SmartComponents/CveDetail/CveDetailPage.js
+++ src/Components/SmartComponents/CveDetail/CveDetailPage.js
@@ -1,12 +1,15 @@
 import React, { useEffect } from 'react';
+import propTypes from 'prop-types';
 import { Main } from '@redhat-cloud-services/frontend-components/Main';
 import CveDetailPageHeader from './CveDetailPageHeader';
 import CveDetailTable from './CveDetailTable';
 import { useSelector } from 'react-redux';
 import ErrorHandler from '../../PresentationalComponents/ErrorHandler';
 import useChrome from '@redhat-cloud-services/frontend-components/useChrome';
+import TableTabs from '../../PresentationalComponents/TableTabs';
+import { CVE_DETAIL_TABS } from '../../../Helpers/constants';
 
-const CveDetailPage = () => {
+const CveDetailPage = ({ activeTab }) => {
   const chrome = useChrome();
 
   const { error, cve } = useSelector(({ CveDetailStore }) => CveDetailStore);
@@ -22,10 +25,23 @@ const CveDetailPage = () => {
     <ErrorHandler error={error}>
       <CveDetailPageHeader />
       <Main>
-        <CveDetailTable />
+        <TableTabs
+          activeTab={activeTab}
+          tabs={[
+            {
+              title: 'Exposed clusters',
+              path: CVE_DETAIL_TABS.clusters,
+              Component: CveDetailTable,
+            },
+          ]}
+        />
       </Main>
     </ErrorHandler>
   );
+};
+
+CveDetailPage.propTypes = {
+  activeTab: propTypes.oneOf(Object.values(CVE_DETAIL_TABS)).isRequired,
 };
 
 export default CveDetailPage;
